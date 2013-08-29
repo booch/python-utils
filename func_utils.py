5f55e49e9a8000cdabd3cf27e701ff8877b4f297
Craig Buchek <github@craigbuchek.com>
2013-08-29 11:36:38 -0500

    Add decorator base class.

--- func_utils.py
+++ func_utils.py
@@ -9,6 +9,58 @@ I don't claim to understand Python decorators well - just well enough to get the
 '''
 
 
+class decorator(object):
+    '''
+        Base class for "polite" decorators.
+        Adapted from https://gist.github.com/kylebgorman/5878715 and http://www.artima.com/weblogs/viewpost.jsp?thread=240845.
+
+        Usage - subclass from decorator, and override _do_():
+            class my_decorator(decorator):
+                def _do_(self, instance, *args, **kwargs):
+                    do_something_before_decorated_function()
+                    result = self.function(instance, *args, **kwargs)
+                    do_something_after_decorated_function()
+                    return result
+    '''
+
+    def __init__(self, *args, **kwargs):
+        if callable(args[0]):
+            self.function = args[0]
+            self.args = list(args).remove(args[0])
+            self.kwargs = kwargs
+        else:
+            self.function = None
+            self.args = args
+            self.kwargs = kwargs
+ 
+    def __doc__(self):
+        return self.function.__doc__
+ 
+    def __repr__(self):
+        return repr(self.function)
+ 
+    def __str__(self):
+        return str(self.function)
+ 
+    def __name__(self):
+        return self.function.__name__
+
+    def __call__(self, *args, **kwargs):
+        if self.function:
+            return self._do_(self, *args, **kwargs)
+        else:
+            self.function = args[0]
+            def wrapped_function(instance, *wrapped_args, **wrapped_kwargs):
+                return self._do_(instance, *wrapped_args, **wrapped_kwargs)
+            return wrapped_function
+
+    # Override this!
+    def _do_(self, instance, *args, **kwargs):
+        return self.function(instance, *args, **kwargs)
+
+    # Define __get__ if you want to make the function behave like a property instead of a method.
+
+
 class memoized_property(object):
     '''
         Simple memoization (caching) decorator for a method that takes no arguments and is used as a property.
